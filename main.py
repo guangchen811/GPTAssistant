e2289b46b4f46183ceff16dead58f5d83ce038e5
guangchen <guangchen811@gmail.com>
2023-04-10 00:22:07 +0800

    multi round chat

--- main.py
+++ main.py
@@ -2,21 +2,60 @@ import os
 import openai
 import sqlite3
 from utils.request import chat_complete
-from utils.writeDB import connect_db, write_db
+from utils.writeDB import connect_db, write_db, process_database_requests
 
 
 if __name__ == "__main__":
     # Create a new chat completion
     openai.api_key = os.getenv("OPENAI_API_KEY")
     conn = connect_db('DB/TextDatabase.db')
+    message_history = [
+        {"role": "system", "content": 
+"""You are a personal assistant. Your responsibility is to answer questions and help the user with their tasks. You have access to your own memory, called Personal Storage Database (PSDB), which is a sqlite3 database created using the command: `CREATE TABLE messages (name, content)`. You can perform the following actions:
+
+1. Save information in the PSDB using the format `database save: INSERT INTO messages VALUES ('name', 'content')`
+2. Retrieve information from the PSDB using the format `database requrest: <sql request>`
+
+When responding to a user's request, please structure your response as follows:
+
+`response to use: <answer> [must be the first line]
+[if necessary]database requrest: <sql request>
+[if necessary]database save: <sql request>`
+
+You don't need to include SQL requests if they're not necessary. Here are some examples:
+
+1. User: 'My favorite color is red. Please remember it.'
+Your response: 'Your favorite color has been recorded.\ndatabase save: INSERT INTO messages VALUES ('color', 'red')'
+
+2. User: 'What is my favorite color? Check in PSDB.'
+Your response: 'I'll send a request to the database to find out.\ndatabase requrest: SELECT content FROM messages WHERE name = 'color''
+
+Keep in mind that this is not your first time using PSDB, so some information about the user might already be stored there. Feel free to use that information to answer the user's questions.
+"""
+}]
+
     while True:
         message = input("Enter your message: ")
         if message == "quit":
             print("This is the end of conversation, brief history has been saved.")
             break
         else:
-            response = chat_complete(message)
-            print(response['choices'][0]['message']['content'])
-            # write_db(conn, name, content)
+            retry_count = 0
+            while retry_count < 3:
+                try:
+                    response = chat_complete(message, message_history)
+                    message_history.append({"role": "user", "content": message})
+                    assistant_response = response['choices'][0]['message']['content']
+                    print(assistant_response)
+                    message_history.append({"role": "assistant", "content": assistant_response})
+                    search_results = process_database_requests(assistant_response, conn)
+                    if search_results:
+                        print("Search results:", search_results)
+                    break
+                except Exception as e:
+                    print("Error:", e)
+                    retry_count += 1
+                    print("Retrying...")
+                
     conn.close()
     print(response)
--- utils/writeDB.py
+++ utils/writeDB.py
@@ -1,4 +1,5 @@
 import sqlite3
+import re
 
 def connect_db(db_path):
     conn = sqlite3.connect(db_path)
@@ -20,3 +21,25 @@ def write_db(conn, name, content):
     conn.execute(f"INSERT INTO messages VALUES ({name}, {content})")
     # commit the changes and close the connection
     conn.commit()
+
+def process_database_requests(response, conn):
+    save_pattern = r"database save: (.*)(?=\s|$)"
+    request_pattern = r"database request: (.*)(?=\s|$)"
+    
+    save_matches = re.findall(save_pattern, response)
+    for match in save_matches:
+        sql = match.strip()
+        print(sql)
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        conn.commit()
+    request_matches = re.findall(request_pattern, response)
+    search_results = []
+    for match in request_matches:
+        sql = match.strip()
+        print(sql)
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        search_results.extend(results)
+    return search_results
