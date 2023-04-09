fce4740f9fdc14bd658c27c1a3f954f70c6f7772
guangchen <guangchen811@gmail.com>
2023-04-09 23:26:33 +0800

    one round chat

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import os
 import openai
+import sqlite3
 from utils.request import chat_complete
+from utils.writeDB import connect_db, write_db
 
 
 if __name__ == "__main__":
     # Create a new chat completion
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    message = "Hello, how are you?"
-    response = chat_complete(message)
+    conn = connect_db('DB/TextDatabase.db')
+    while True:
+        message = input("Enter your message: ")
+        if message == "quit":
+            print("This is the end of conversation, brief history has been saved.")
+            break
+        else:
+            response = chat_complete(message)
+            print(response['choices'][0]['message']['content'])
+            # write_db(conn, name, content)
+    conn.close()
     print(response)
--- utils/writeDB.py
+++ utils/writeDB.py
@@ -1,15 +1,22 @@
 import sqlite3
 
-# create a new database file
-conn = sqlite3.connect('DB/TextDatabase.db')
+def connect_db(db_path):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
 
-# create a table
-conn.execute('CREATE TABLE contacts (name text, email text)')
+    # Check if the messages table exists
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='messages'")
+    table_exists = cursor.fetchone()
 
-# insert data into the table
-conn.execute("INSERT INTO contacts VALUES ('John Doe', 'john@example.com')")
-conn.execute("INSERT INTO contacts VALUES ('Jane Smith', 'jane@example.com')")
+    # If the table doesn't exist, create it
+    if not table_exists:
+        cursor.execute('CREATE TABLE messages (name, content)')
+        conn.commit()
 
-# commit the changes and close the connection
-conn.commit()
-conn.close()
+    return conn
+
+def write_db(conn, name, content):
+    # insert data into the table
+    conn.execute(f"INSERT INTO messages VALUES ({name}, {content})")
+    # commit the changes and close the connection
+    conn.commit()
